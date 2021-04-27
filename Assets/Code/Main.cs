8d5e0a55a147997916d8206ed85c1fe8dae90475
Casey Brant <casey@caseybrant.com>
2021-04-27 11:12:10 -0500

    let's go

---
+++ Assets/Code/Main.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Booting up...");
+    }
+
+    void Update()
+    {
+        
+    }
+}
